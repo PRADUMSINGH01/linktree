9656b8a90e5a7b24ae95ee5c810505237fe6e308
harshit singh <harshit07029@gmail.com>
2025-01-13 14:40:46 +0000

    middleware

--- app/middleware.js
+++ app/middleware.js
@@ -1,11 +1,13 @@
+"use server";
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 const JWT_SECRET = new TextEncoder().encode(process.env.NEXT_SECRET); // Secret key
 
 export async function middleware(req) {
+  console.log("Middleware is running..."); // Debug log
+
   const token = req.cookies.get("token");
-  
   const currentPath = req.nextUrl.pathname;
 
   const authRoutes = ["/login", "/register"];
@@ -20,7 +22,15 @@ export async function middleware(req) {
       response.cookies.set("token", "", { maxAge: -1 }); // Clear token
       return response;
     }
-  } else if (token) {
+  }
+
+  // If no token is found, redirect to the register page (except for /login and /register)
+  if (!token) {
+    if (currentPath !== "/register" && currentPath !== "/login") {
+      return NextResponse.redirect(new URL("/register", req.url));
+    }
+  } else {
+    // If there's a token, validate it for non-auth routes
     try {
       await jwtVerify(token, JWT_SECRET);
     } catch (err) {
@@ -35,5 +45,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-  matcher: ["/login", "/register", "/", "/Dashboard"], // Protect relevant routes
+  matcher: ["/"], // Match specific routes or all routes (e.g. '/')
 };
